f613ef4060fbf768c78e6f2a2ded90001aaa1d59
Sh0uf <144715426+Sh0uf@users.noreply.github.com>
2025-03-06 10:28:07 +0000

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Swanky_and_Moo_Moo } from 'next/font/google'
+
+const swankyAndMooMoo = Swanky_and_Moo_Moo({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-swanky',
+})
 
 export const metadata: Metadata = {
   title: "Fortuna Salong",
